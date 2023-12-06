6f8cf4229255734dab1193230f95187b9b20bb96
Marcos Quinteros <marcos.quinteros2003@gmail.com>
2023-12-06 11:05:44 -0300

    O 3 a 6 paneles simples con totales. (FALTA EL DE USER) ○ Panel de detalle de último producto ○ Panel de categorías con el total de productos de cada una. ○ Panel con el listado de productos.

--- src/components/LastMovieInDb.jsx
+++ src/components/LastMovieInDb.jsx
@@ -1,24 +1,65 @@
-import React from 'react';
-import imagenFondo from '../assets/images/mandalorian.jpg';
+import React, { useEffect, useState } from "react";
+import NotFound from "./NotFound";
 
 function LastMovieInDb(){
-    return(
-        <div className="col-lg-6 mb-4">
-            <div className="card shadow mb-4">
-                <div className="card-header py-3">
-                    <h5 className="m-0 font-weight-bold text-gray-800">Ultimo Producto Creado</h5>
-                </div>
-                <div className="card-body">
-                    <div className="text-center">
-                        <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 40 +'rem'}} src={imagenFondo} alt=" Star Wars - Mandalorian "/>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa citationem ratione aperiam voluptatum non corporis ratione aperiam voluptatum quae dolorem culpa ratione aperiam voluptatum?</p>
-                    {/* <a className="btn btn-danger" target="_blank" rel="nofollow" href="/">View movie detail</a> */}
-                </div>
-            </div>
-        </div>
+    const [lastProduct, setLastProduct] = useState(null);
 
-    )
+  useEffect(() => {
+    fetch("http://localhost:4200/api/products/list")
+      .then((response) => response.json())
+      .then((data) => {
+        // Obtener el último producto de la lista
+        const latestProduct = data.data[data.data.length - 1];
+        setLastProduct(latestProduct);
+      })
+      .catch((error) => {
+        <NotFound/>
+      });
+  }, []);
+
+  if (!lastProduct) {
+    return <div>Cargando...</div>; 
+  }
+
+  const capitalizeFirstLetter = (str) => {
+    return str.replace(/\b\w/g, (char) => char.toUpperCase());
+  };
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center" style={{ marginTop: "20px" }}>
+        <div className="col-lg-12 text-center mb-4">
+          <h1>Detalle del Último Producto</h1>
+        </div>
+      </div>
+
+      <div className="row justify-content-center">
+        <div className="card shadow mb-4" style={{ margin: "10px" }}>
+          <div className="card-header py-3">
+            <h5 className="m-0 font-weight-bold text-gray-800">
+              {capitalizeFirstLetter(lastProduct.name)}
+            </h5>
+          </div>
+          <div className="card-body">
+            <div className="text-center">
+              <img
+                className="img-fluid px-3 px-sm-4 mt-3 mb-4"
+                style={{ width: "40rem" }}
+                src={lastProduct.image}
+                alt={lastProduct.name}
+              />
+            </div>
+            <p>Descripción: {capitalizeFirstLetter(lastProduct.description)}</p>
+            <p>Precio: {lastProduct.price}</p>
+            <p>Categoría: {capitalizeFirstLetter(lastProduct.category)}</p>
+            <p>Género: {capitalizeFirstLetter(lastProduct.gender)}</p>
+            <p>Stock: {lastProduct.amount}</p>
+            <p>Descuento: {lastProduct.discount}%</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LastMovieInDb;
